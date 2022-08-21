7574d11fadae8e3cd711e1f77073d96a1962c006
Mahmoud <elmshadmahmoud09@gmail.com>
2022-08-21 17:35:10 +0200

    Empty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import "./App.css";
 import Cart from "./Components/Cart/cart";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-  Link,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/home";
 import Details from "./Components/Details/details";
 import Header from "./Components/Header/header";
-import Header2 from "./Components/noon-nav/Header/header";
+import React from "react";
 import Footer from "./Components/Footer/footer";
 import Sidebar from "./Components/Side-bar/side-bar";
-import NoonNavbar from "./Components/noon-nav/Header/navbar";
+
 import { AuthProvider } from "./Auth";
 import Login from "./Components/Sign-in/Login";
 import Signup from "./Components/Sign-up/Signup";
-import PrivateRoute from "./PrivateRoute";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 import Information from "./Components/Header/Information";
@@ -45,15 +39,7 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route path="sidebar" element={<Sidebar />} />
-              <Route
-                exact
-                path="cart"
-                element={
-                  <ProtectedRoute>
-                    <Cart />
-                  </ProtectedRoute>
-                }
-              />
+              <Route exact path="cart" element={<Cart />} />
               <Route path="details" element={<Details />} />
               <Route path="login" element={<Login />} />
               <Route path="signup" element={<Signup />} />
--- src/Components/axios/axiosConfig.js
+++ src/Components/axios/axiosConfig.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 import db from "../../firebase";
-import { collection } from 'firebase/firestore';
+import { collection } from "firebase/firestore";
 
-
-
-
-const prdCollection = collection(db, 'Mobile')
+const prdCollection = collection(db, "Mobile");
 export const axiosInstance = axios.create({
-    baseURL:`${prdCollection}`
-
+  baseURL: `${prdCollection}`,
 });
